8050513ab38bab95de5a9dc1a91095e5d53b36b0
Harsh Wasnik <harshwasnik160602@gmail.com>
2022-10-28 23:59:40 +0530

    added urls

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,20 @@
 from django.urls import path
 from django.urls import include
 from app import views
+from .views_market import *
+user_patterns = [
+    path('create/', views.create, name='create'),
+    path('portfolio/<int:pk>', views.portfolio, name='portfolio'),
+]
+market_patterns = [
+    path('price/', marketprice, name='price'),
+    path('orderbook/', orderbook, name='orderbook'),
+    path('tradehistory/', tradehistory, name='tradehistory'),
+    path('trade/', trade, name='trade'),
+]
 urlpatterns = [
     path('', views.index, name='index'),
+    path('user/', include(user_patterns)),
+    path('market/', include(market_patterns)),
 ]
+
