bdc22f559db8b04a6de7deb05062284505dfc18f
harshverma250202 <harshverma250202@gmail.com>
2022-10-29 23:10:23 +0530

    Co-authored-by: Harsh Wasnik <MUCCHU@users.noreply.github.com> Co-authored-by: vedprakash182001 <vedprakash182001@users.noreply.github.com>

--- app/serializers.py
+++ app/serializers.py
@@ -14,6 +14,10 @@ class LimitOrderSerializer(serializers.ModelSerializer):
         model = LimitOrder
         fields = '__all__'
 
+    def to_representation(self, instance):
+        self.fields['user'] = UserSerializer(read_only=True)
+        return super(LimitOrderSerializer, self).to_representation(instance)
+
 
 class CurrentMarketPriceSerializer(serializers.ModelSerializer):
     class Meta:
@@ -25,3 +29,8 @@ class TradeHistorySerializer(serializers.ModelSerializer):
     class Meta:
         model = TradeHistory
         fields = '__all__'
+
+    def to_representation(self, instance):
+        self.fields['user1'] = UserSerializer(read_only=True)
+        self.fields['user2'] = UserSerializer(read_only=True)
+        return super(TradeHistorySerializer, self).to_representation(instance)
